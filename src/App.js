8c71caf694def291cc852131d1fda85cf62709d5
Valerii from earth <lvaldev.code@gmail.com>
2022-08-09 23:08:01 +0200

    add sorting

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import "./App.css";
 
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
+import Select from "./components/UI/Select/Select";
 
 function App() {
 	const [posts, setPosts] = useState([
-		{ id: 1, title: "JavaScript", text: "JavaScript is a programming language" },
-		{ id: 2, title: "React", text: "React is the JavaScript library" },
-		{ id: 3, title: "Angular", text: "Angular is the JavaScript framework" },
+		{ id: 1, title: "JavaScript", description: "A JavaScript is a programming language" },
+		{ id: 2, title: "React", description: "C React is the JavaScript library" },
+		{ id: 3, title: "Angular", description: "B Angular is the JavaScript framework" },
 	]);
+	const [selectedSort, setSelectedSort] = useState('')
+
 
 	const createPost = (newPost) => {
 		setPosts([...posts, newPost]);
@@ -19,10 +22,27 @@ function App() {
 	const removePost = (post) => {
 		setPosts(posts.filter((p) => p.id !== post.id));
 	};
+
+	const sortPosts=(sort)=>{
+		setSelectedSort(sort);
+		setPosts([...posts].sort((a,b)=>a[sort].localeCompare(b[sort])))
+
+	}
 	return (
 		<div className='App'>
 			<PostForm create={createPost} />
-			{posts.length !== 0 ? <PostList posts={posts} title='Posts list' remove={removePost} /> : <h3>Have no posts!</h3>}
+			<Select defaultValue='Sort by'
+					value={selectedSort}
+					onChange={sortPosts}
+					options={[
+				{value:'title',name:'By name'},
+				{value:'description',name:'By description'},
+			]}
+			/>
+			{posts.length !== 0 ?
+				<PostList posts={posts} title='Posts list' remove={removePost} /> :
+				<h3>Have no posts!</h3>
+			}
 		</div>
 	);
 }
---
+++ src/components/UI/Select/Select.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Select = ({options, defaultValue, value, onChange}) => {
+    return (
+            <select value={value}
+                    onChange={e => onChange(e.target.value)}>
+                        <option value='' disabled>{defaultValue}</option>
+                        {options.map(option=>
+                        <option key={option.value} value={option.value}>{option.name}</option>
+                )}
+            </select>
+    );
+};
+
+export default Select;
