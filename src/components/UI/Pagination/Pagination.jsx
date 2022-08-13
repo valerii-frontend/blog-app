1cb3f27f5a3513d53bfc541a0d0c544a627b8bfb
Valerii from earth <lvaldev.code@gmail.com>
2022-08-13 16:42:00 +0200

    change styles

--- src/components/UI/Pagination/Pagination.jsx
+++ src/components/UI/Pagination/Pagination.jsx
@@ -7,7 +7,7 @@ function Pagination({totalPages, page,changePage}) {
     return (
         <div className={styles.pagination}>
             {pagesCount.map(number =>
-                <button key={number} className={page === number?'active':''} onClick={()=> changePage(number)}>
+                <button key={number} className={`${styles.btn} ${page === number?styles.active:''}`} onClick={()=> changePage(number)}>
                     {number}
                 </button>
             )}
